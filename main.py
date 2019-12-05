8a920f06257d1d606bff103e192d76ad53c5dc22
Maria Pritchina <deco-bots@yandex.ru>
2019-12-05 15:58:46 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+from preparation.data_base import DataBase
+from preparation.import_text_to_db import import_text_to_db
+
+if __name__ == '__main__':
+    src = 'src/GranatoviyBraslet.txt'
+    db = DataBase('books')
+    ss = db.session
+    import_text_to_db(ss=ss, src=src)
